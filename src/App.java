1e6b17e73a0b4c2d57824b653d2a14749736f2ec
Janosch Walter <janowalter@tsn.at>
2024-09-24 11:28:23 +0200

    Klassen von IntellijVariante hinzugefügt.

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class App {
+
+	public static void main(String[] args)
+	{
+		Medienbibliothek bib = new Medienbibliothek();
+		
+		VHS heman = new VHS("He-Man and the Masters of the Universe", 120.99, "B�ro, UG", 120, "Ausgezeichnet");
+		Cartridge zeldaGold = new Cartridge("The Adventures of Zelda", 300.00, "Keller, UG", true, "Nintendo", Konsolentyp.NES);
+		Buch herr = new Buch("Herr der Ringe", 49.99, "Wohnzimmer, OG", "1234565432123");
+		
+		bib.mediumHinzufuegen(heman);
+		bib.mediumHinzufuegen(zeldaGold);
+		bib.mediumHinzufuegen(herr);
+		
+		bib.alleMedienAusgeben();
+		
+		System.out.println("");
+		System.out.println("--- alle VHS in der Liste ---");
+		for(VHS vhs : bib.alleVHSalsListe())
+		{
+			vhs.anzeigen();
+		}
+	}
+}
